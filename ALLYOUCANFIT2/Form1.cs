2b83a608d83e5715a95f160cf00c765f67dc1b99
Tristan Gabriel N Espiritu <tristanrielespiritu@gmail.com>
2025-11-29 19:54:32 +0800

    Add files via upload

---
+++ ALLYOUCANFIT2/Form1.cs
@@ -0,0 +1,53 @@
+namespace ALLYOUCANFIT2
+{
+    public partial class submitBtn : Form
+    {
+        public submitBtn()
+        {
+            InitializeComponent();
+        }
+        //Age
+        private void Age_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //Weight
+        private void Weight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //Height
+        private void Height_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //LOSE WEIGHT BUTTON
+        private void btnLose_Click(object sender, EventArgs e)
+        {
+
+        }
+        //MAINTAIN WEIGHT BUTTON
+        private void btnMaintain_Click(object sender, EventArgs e)
+        {
+
+        }
+        //GAIN WEIGHT BUTTON
+        private void btnGain_Click(object sender, EventArgs e)
+        {
+
+        }
+        //submit button
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ ALLYOUCANFIT2/Form2.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ALLYOUCANFIT2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            SuspendLayout();
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(482, 726);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
